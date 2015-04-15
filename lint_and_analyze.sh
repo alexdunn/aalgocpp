a834ece18296e64df70992a9d44ad8aa5828208c
Alex Dunn <dunn.alex@icloud.com>
2015-04-15 01:32:12 -0700

    Add clang static analysis, clang tidy, and flawfinder

---
+++ lint_and_analyze.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# This runs all checks that are available to clang-tidy, which is a superset of the checks that are run by clang's static analyzer
+
+set -e
+
+cd src
+for filename in ./*; do
+    echo -e "\n\n=== Linting and analyzing $filename"
+    clang-tidy-3.6 -p ../bii/build/ -checks='*' "$filename"
+    #cppcheck "$filename"
+    echo -e '\x1B[0;31m'
+    flawfinder -Q -D --omittime --quiet "$filename"
+    echo -e '\x1B[0m'
+done
+cd ..
+
--- src/sorting.hpp
+++ src/sorting.hpp
@@ -7,5 +7,5 @@ namespace aalgo
 // Take an array of ints and return that array sorted from least to greatest
 std::vector<int> insertionSort(std::vector<int> values);
 
-}
+} // aalgo
 
