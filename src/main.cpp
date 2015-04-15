6460698c02db24ec621e69bee83822bb14769fa1
Alex Dunn <dunn.alex@icloud.com>
2015-04-15 02:27:15 -0700

    Remove address sanitizer test code
    It successfully caught the memory leak.

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,7 @@
 #include "gtest/gtest.h"
 
-void *p;
-
 int main (int argc, char *argv[])
 {
-    p = malloc(7);
-    p = 0; // The memory is leaked here.
-
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
